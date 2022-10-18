99347086be58e457b032918ca949d73b712fe5a1
dus532 <dus532@naver.com>
2022-10-18 23:35:05 +0900

    Feat: app에 version 추가

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as pkg from './package.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test</Text>
+      <Text>{pkg.version}</Text>
       <StatusBar style='auto' />
     </View>
   );
