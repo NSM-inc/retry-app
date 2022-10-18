8c9a5de32ab824a488ea33254ed2d2c8dee96300
dus532 <dus532@naver.com>
2022-10-18 23:12:06 +0900

    Fix: app.config.js 수정

--- app.config.js
+++ app.config.js
@@ -1,36 +1,34 @@
 import * as pkg from './package.json';
 
 module.exports = {
-  expo: {
-    name: 'retry-app',
-    slug: 'retry-app',
-    version: pkg.version,
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    userInterfaceStyle: 'light',
-    runtimeVersion: { policy: 'nativeVersion' },
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
+  name: 'retry-app',
+  slug: 'retry-app',
+  version: pkg.version,
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  runtimeVersion: { policy: 'nativeVersion' },
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.nsm.retryapp',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
     },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ['**/*'],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: 'com.nsm.retryapp',
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#FFFFFF',
-      },
-      package: 'com.nsm.retryapp',
-    },
-    web: {
-      favicon: './assets/favicon.png',
-    },
+    package: 'com.nsm.retryapp',
+  },
+  web: {
+    favicon: './assets/favicon.png',
   },
 };
