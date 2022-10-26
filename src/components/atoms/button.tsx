d44bd459824f32a1f0fe4b918717bd8d70d2f994
imzeze <jihyekim019@gmail.com>
2022-10-26 09:58:40 +0900

    ing: atom designType 사용

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,19 +1,17 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { COLOR } from 'resources/constants';
+import styled from 'styled-components/native';
 
-const Button2 = () => {
+const Button = () => {
   return (
-    <TouchableOpacity style={styles.red} onPress={() => {}}>
+    <TouchableOpacity onPress={() => {}}>
       <Text>Button</Text>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  red: {
-    backgroundColor: COLOR.PRIME,
-  },
-  yellow: {},
-});
+const Box = styled.TouchableOpacity<{ designType: string; size: string }>`
+  ${({ designType }) => 'color: red'}
+`;
 
-export default Button2;
+export default Button;
